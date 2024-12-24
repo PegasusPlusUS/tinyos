0a952b2fde17fbf75c87425a4de8cdb4dcec2b84
Zhenping Zhao <pegasus.cadence@gmail.com>
2024-12-24 09:44:52 -0800

    Refine BIOS routine names.

--- ch01_bootsector/nim/bootsector.nim
+++ ch01_bootsector/nim/bootsector.nim
@@ -2,7 +2,7 @@ include common_bios
 
 proc start() =
   BIOS_CLEAR_SCREEN()
-  BIOS_SET_CURSOR_POS_p_ROW_COL(10, 26)
-  BIOS_PRINT_STRING_p_MSG("Hi, bare-metal world by Nim!")
+  BIOS_SET_CURSOR_POS_P_ROW_COL(10, 26)
+  BIOS_PRINT_STRING_P_MSG("Hi, bare-metal world by Nim!")
 
 start()
