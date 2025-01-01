1d6db52a8b8cd96a409ce2c03440032c1e0ee4aa
Zhenping Zhao <pegasus.cadence@gmail.com>
2025-01-01 15:53:00 -0800

    avoid being optimized

--- ch01_bootsector/v/common_bios.v
+++ ch01_bootsector/v/common_bios.v
@@ -1,6 +1,38 @@
 module common_bios
 
-pub fn asm_bios_clear_screen() {}
-pub fn asm_bios_set_cursor_pos(row u8, col u8) {}
-pub fn asm_bios_set_print_color(c u8) {}
-pub fn asm_bios_print_char(c u8) {}
+pub fn asm_bios_clear_screen() {
+    mut s := 0;
+    for x in 1..5 {
+        s += x;
+    }
+    if s > 0 {
+
+    }
+}
+pub fn asm_bios_set_cursor_pos(row u8, col u8) {
+    mut s := 0;
+    for x in 1..5 {
+        s += x;
+    }
+    if s > 0 {
+
+    }
+}
+pub fn asm_bios_set_print_color(c u8) {
+    mut s := 0;
+    for x in 1..5 {
+        s += x;
+    }
+    if s > 0 {
+
+    }
+}
+pub fn asm_bios_print_char(c u8) {
+    mut s := 0;
+    for x in 1..5 {
+        s += x;
+    }
+    if s > 0 {
+
+    }
+}
