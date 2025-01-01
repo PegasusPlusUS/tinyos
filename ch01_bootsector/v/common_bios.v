85719a659e6ef03143238520eb4425f387e4b4e8
Zhenping Zhao <pegasus.cadence@gmail.com>
2025-01-01 15:57:00 -0800

    Try another way to avoid being optimized

--- ch01_bootsector/v/common_bios.v
+++ ch01_bootsector/v/common_bios.v
@@ -1,38 +1,14 @@
 module common_bios
 
 pub fn asm_bios_clear_screen() {
-    mut s := 0;
-    for x in 1..5 {
-        s += x;
-    }
-    if s > 0 {
-
-    }
+    println('Hello');
 }
 pub fn asm_bios_set_cursor_pos(row u8, col u8) {
-    mut s := 0;
-    for x in 1..5 {
-        s += x;
-    }
-    if s > 0 {
-
-    }
+    println('Hello');
 }
 pub fn asm_bios_set_print_color(c u8) {
-    mut s := 0;
-    for x in 1..5 {
-        s += x;
-    }
-    if s > 0 {
-
-    }
+    println('Hello');
 }
 pub fn asm_bios_print_char(c u8) {
-    mut s := 0;
-    for x in 1..5 {
-        s += x;
-    }
-    if s > 0 {
-
-    }
+    println('Hello');
 }
